b6069f85d9f2ec69be22db5c777bc6f6480b30ad
keitayamaoka <62986434+keitayamaoka@users.noreply.github.com>
2020-05-08 18:25:11 +0900

    Realtimedatabaseとのデータ接続

--- src/main/java/com/seattleacademy/team20/skillController.java
+++ src/main/java/com/seattleacademy/team20/skillController.java
@@ -1,19 +1,39 @@
 package com.seattleacademy.team20;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 @Controller
 public class skillController {
 
-	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(skillController.class);
+	private static final Logger logger = LoggerFactory.getLogger(skillController.class);
 
 	@RequestMapping(value = "/skillUpload" , method = RequestMethod.GET)
 	public String skillUpload(Locale locale, Model model) {
@@ -26,6 +46,67 @@ public class skillController {
 
 		model.addAttribute("serverTime", formattedDate );
 
+
+		try {
+				initialize();
+		} catch (IOException e) {
+				e.printStackTrace();
+		};
+		List<SkillCategory> categories = selectSkillCategories();
+
+		uploadSkill(categories);
+
 		return "skillUpload";
+	}
+
+
+@Autowired
+
+private JdbcTemplate jdbcTemplate;
+
+public List<SkillCategory> selectSkillCategories() {
+	final String sql = "select * from skills";
+	return jdbcTemplate.query(sql, new RowMapper<SkillCategory>() {
+			public SkillCategory mapRow(ResultSet rs, int rowNum) throws SQLException{
+					return new SkillCategory(rs.getInt("id"), rs.getString("category"), rs.getString("name"), rs.getInt("score"));
+			}
+	});
+}
+
+private FirebaseApp app;
+
+public void initialize() throws IOException {
+	FileInputStream refreshToken = new FileInputStream("/Users/yamaokatakashi/key/dev-portfolio-db7ac-firebase-adminsdk-pzlqc-94cb3456e5.json");
+	FirebaseOptions options = new FirebaseOptions.Builder()
+		.setCredentials(GoogleCredentials.fromStream(refreshToken))
+		.setDatabaseUrl("https://dev-portfolio-db7ac.firebaseio.com/")
+		.build();
+	app = FirebaseApp.initializeApp(options, "other");
+}
+
+public void uploadSkill(List<SkillCategory> categories) {
+		final FirebaseDatabase database = FirebaseDatabase.getInstance(app);
+		DatabaseReference ref = database.getReference("skills");
+
+
+		List<Map<String, Object>> dataList = new ArrayList<Map<String,Object>> ();
+		Map<String, Object> dataMap;
+		Map<String,List<SkillCategory>>skillMap =  categories.stream().collect(Collectors.groupingBy(SkillCategory::getCategory));
+		for(Map.Entry<String,List<SkillCategory>> entry : skillMap.entrySet()) {
+			dataMap = new HashMap<>();
+			dataMap.put("category", entry.getKey());
+			dataMap.put("skill", entry.getValue());
+			dataList.add(dataMap);
+		}
+		ref.setValue(dataList, new DatabaseReference.CompletionListener() {
+				@Override
+				public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+					if(databaseError != null) {
+							System.out.println("Data could be saved" + databaseError.getMessage());
+					} else {
+							System.out.println("Data save successfully.");
+					}
+				}
+		});
 	}
 }
